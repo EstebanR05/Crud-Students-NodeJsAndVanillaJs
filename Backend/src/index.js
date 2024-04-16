4799cb10553d742c1cef4b549651e3a61b689949
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-04-15 20:58:39 -0500

    se desintegran errores en los paquetes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,6 @@
 //get librarys
 const express = require('express');
-const mongoose = require('mongoose');
-const studentRoute = require('./routes/StudentRoute.js');
+//const studentRoute = require('./routes/StudentRoute.js');
 
 //variables
 const app = express();
@@ -10,12 +9,9 @@ const bodyParse = require('body-parser');
 
 //using express
 app.use(bodyParse.json());
-app.use('/', studentRoute);
-
-//using mongoose for implement the base of date
-mongoose.connect(urlMongo, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(error => console.error('Error connecting to MongoDB:', error));
+app.use('/', (req, res) => {
+    res.send('hey peaple!')
+});
 
 //port when start the proyect    
 app.listen(PORT, () => console.log("Server listening on port:", PORT));
