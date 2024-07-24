f45924b960ee06312aa9f898160aa3d76206dbb4
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2024-07-24 14:46:25 -0500

    se integran cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,16 +1,29 @@
-import express from 'express';
-import studentRoute from './routes/StudentRoute.js';
-import bodyParser from 'body-parser';
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookiesParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
 
-//variables
+import studentRoute from "../src/routes/StudentRoute.js";
+
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const _PORT = process.env.PORT_APPLICATION;
 
-//using express
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookiesParser());
 app.use(bodyParser.json());
 
 //endPoints
 app.use('/', studentRoute);
 
 //port when start the proyect    
-app.listen(PORT, () => console.log("Server listening on port:", PORT));
+const server = http.createServer(app);
+
+server.listen(_PORT, () => {
+  console.log(`Server running on http://localhost:${_PORT}/api`);
+});
